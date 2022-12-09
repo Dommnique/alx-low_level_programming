cb7ffe25a8c3a0a5e6be07d0c79455891f75a2c7
dommnique <dommnicomeke@gmail.com>
2022-12-08 19:27:47 -0800

    Added a printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+printf("With proper grammar,but the outcome is a piece of art,\n");
+return (0);
+}
