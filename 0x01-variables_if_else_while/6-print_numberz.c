dd7433e2e5f006b7bdc7c37ad75a256d31c52490
dommnique <dommnicomeke@gmail.com>
2022-12-10 05:23:13 -0800

    Added a print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * msin - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return (0);
+ */
+int main(void)
+{ 
+	int digits;
+
+	for (digits = 0; digits <= 9; digits++)
+		printchar('digits');
+
+	printchar('\n);
+
+	return (0);
