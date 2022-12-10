a25264cae44d76d6cf074c2f586df1a51e96486e
dommnique <dommnicomeke@gmail.com>
2022-12-10 04:46:15 -0800

    Added a print-numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always (0)
+ */
+int main (void)
+{
+	int digits;
+
+	for (digits = 0; digits <= 9; digits++);
+	{
+		printf("new digit is : %d\n", digits);
+	}
+
+	return (0);
+}
