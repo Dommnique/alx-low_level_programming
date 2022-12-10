37e572fdc8636af68f61b7511aafdd647141abe2
dommnique <dommnicomeke@gmail.com>
2022-12-09 20:01:12 -0800

    Added a print_alphabet.c file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  *
  * Return: Always (0).
  */
-
-int main(void){
+int main(void)
+{
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-				putchar(letter);
+		putchar(letter);
 
 	putchar('\n');
 
