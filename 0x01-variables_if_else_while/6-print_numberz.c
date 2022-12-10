723c435b980bea6f2fef0ab90714988ae97065d9
dommnique <dommnicomeke@gmail.com>
2022-12-10 06:00:04 -0800

    Added a print_numberz.c file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * msin - prints all single digit numbers of base 10 starting from 0
+ * main - prints all single digit numbers of base 10 starting from 0
  *
- * Return (0);
+ * Return: (0).
  */
 int main(void)
 { 
