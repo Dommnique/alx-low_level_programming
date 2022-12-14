3a86a7c6b70ffae16f233e27f57a36b8c6fb9d05
dommnique <dommnicomeke@gmail.com>
2022-12-13 18:42:05 -0800

    Added a putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * main - Entry point
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+	char cat = "_putchar";
+
+	while (cat)
+	{
+		_putchar(cat);
+		cat++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
