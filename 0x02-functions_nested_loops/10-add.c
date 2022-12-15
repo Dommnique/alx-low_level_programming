12d1c0a9aaa25820b07bdff9c2b14dcb28bc97c9
dommnique <dommnicomeke@gmail.com>
2022-12-14 20:42:00 -0800

    Added an add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @num1: first number input
+ * @num2: second number input
+ * Return: num1 + num2
+ */
+
+int add(int num1, int num2)
+{
+	int num1, num2;
+	int result = num1 + num2;
+
+	_putchar(num1 + num2);
+	return (result);
+}
