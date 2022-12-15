60a4b444e6d16219ef4818945cf216dffe5f0e4b
dommnique <dommnicomeke@gmail.com>
2022-12-15 11:20:41 -0800

    Added a main.c file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+fndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+printf("Infinite loop incoming :(\n");
+printf("%d is the largest number\n", largest);
+printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+#endif 
