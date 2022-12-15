fb832a0689e0a2f59f8d6c2868c864218610dbae
dommnique <dommnicomeke@gmail.com>
2022-12-14 18:09:05 -0800

    Added a print_alphabet_x10.c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - print alphabet 10 times
+ * print_alphabet_x10 - print alphabet 10 times
  */
 
 void print_alphabet_x10(void)
 {
 	int n, repeat;
+
 	repeat = 0;
- 
+
 	while (repeat < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
