acfa57e2982ef177e5ae69689b65a2d1b6e76f9e
dommnique <dommnicomeke@gmail.com>
2022-12-14 17:27:09 -0800

    Added an alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always(0).
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
