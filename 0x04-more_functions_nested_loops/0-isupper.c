32423e9deb8737ae7ad297dd4e38a0a521133ccd
dommnique <dommnicomeke@gmail.com>
2022-12-16 10:53:16 -0800

    Added  0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: Character input
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c == 'C')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
