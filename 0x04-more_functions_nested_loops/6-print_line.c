d4ba1d3791024bcce99bf777902b3febe4f0ef87
dommnique <dommnicomeke@gmail.com>
2022-12-16 19:39:29 -0800

    Added 6-print_line.c file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,15 @@ void print_line(int n)
 {
 	int a = 0;
 
-	while (a <= n)
+	if (n > 0)
 	{
-		if (n > 0)
+		while (a <= n)
 		{
-			_putchar('\n');
+			_putchar('_');
+			a++;
 		}
-
-	_putchar ('_');
-	a++;
+		 _putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
