6adbf709e349e5c00dca5f460ddc3ceb16ec2437
dommnique <dommnicomeke@gmail.com>
2023-01-21 06:35:30 -0800

    Added 1-print_numbers.c file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,9 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d\n", va_arg(mylist, int));
+		printf("%d", va_arg(mylist, int));
 
-		if (separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
