6e1f8cbb544e531110cd13c364c3b1a3d59d9b25
dommnique <dommnicomeke@gmail.com>
2023-01-31 19:49:26 -0800

    Added a new file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head node
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	while (head)
+	{
+		listint_t *tmp = head;
+
+		head = head->next;
+		free(tmp);
+	}
+}
